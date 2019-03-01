4123fefb43414b3061c0ebaada824169013374e4
pocketdora <Wade@Lizs-MacBook-Air.local>
2019-03-01 07:37:20 +0000

    updated with my own code

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,39 @@
 import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    return pd.merge(messages, categories, on='id')
 
 
 def clean_data(df):
-    pass
+
+    categories = df['categories'].str.split(';', expand=True)
+    row = categories.iloc[0]
+    category_colnames = row.apply(lambda x : x[:-2])
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda x : x[-1])
+        # convert column from string to numeric
+        categories[column] = categories[column].apply(lambda x: int(x))
+
+    df.drop('categories', axis=1, inplace=True)
+    df = pd.concat([df, categories], axis=1)
+    df.drop_duplicates('original', inplace=True)
+    return df
+
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///' + database_filename + '.db') 
+    df.to_sql(database_filename, engine, index=False, if_exists='replace')
 
 
 def main():
@@ -24,12 +47,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
